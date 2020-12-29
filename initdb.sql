c998d27f8a894d1486082204493fd0e2444420f5
Maura Hurley <G00376337@gmit.ie>
2020-12-29 12:56:08 +0000

    updated initdb.sql

--- initdb.sql
+++ initdb.sql
@@ -1,9 +1,18 @@
 USE DATAREPRESENTATION;
 
 
-create table books(
-    -> ISBN int PRIMARY KEY,
-    -> title varchar(250),
-    -> author varchar(250),
-    -> price int
+create table shares(
+    -> id INT NOT NULL AUTO_INCREMENT,
+    -> symbol VARCHAR(30),
+    -> name VARCHAR(250),
+    -> price INTEGER,
+    -> PRIMARY KEY (id)
+    -> );
+
+  
+create table buyer(
+    -> buyerid INT NOT NULL AUTO_INCREMENT,
+    -> region VARCHAR(80),
+    -> PRIMARY KEY (buyerid),
+    -> FOREIGN KEY (buyerid) REFERENCES shares(id)
     -> );
